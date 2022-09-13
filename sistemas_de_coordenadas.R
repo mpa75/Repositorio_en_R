21edb405740d6d2e202139c6811db9e06cc1fac7
unknown <mpa75@alu.ua.es>
2022-09-13 15:54:24 +0200

    Ultimos cambios

--- sistemas_de_coordenadas.R
+++ sistemas_de_coordenadas.R
@@ -71,3 +71,9 @@ bar <- ggplot(data = diamonds) +
 
 bar + coord_flip()
 bar + coord_polar()
+
+ggplot(data = mpg, mapping = aes(x = cty, y = hwy)) +
+  geom_point() + 
+  geom_abline() +
+  coord_fixed() +
+  geom_jitter()
