ad69469c3efa8d82e538645200e362754831f111
unknown <mpa75@alu.ua.es>
2022-10-04 15:12:22 +0200

    Practica 1 hasta el ejercicio 3

---
+++ Master/Practica_1.R
@@ -0,0 +1,27 @@
+
+
+#Ejercicio 1
+
+nombre <- "Manuel peiró Agulló"
+estatura <- 1.96
+num_leng <- "C, C++, Java, SQL"
+fumador <- FALSE
+
+class(nombre)
+class(estatura)
+class(num_leng)
+class(fumador)
+#--------------------------------------------------
+
+#Ejercicio 2
+
+numeros <- c(0:29)
+numeros_condicion <- c((pares_buenos <- (pares <- numeros[(numeros %% 2 == 0)])[(pares <- numeros[(numeros %% 2 == 0)]) < 10]), (impares_buenos <- (impares <- numeros[(!numeros %% 2 == 0)])[(impares <- numeros[(!numeros %% 2 == 0)]) > 20]))
+
+#---------------------------------------------------
+
+#Ejercicio 3
+
+progresion <- c((2*3)^(a <- c(0:10)))
+indice <- progresion[(progresion %% 9 == 0)]
+indices_condicion <- which((indice <- progresion[(progresion %% 9 == 0)]) < 100)
