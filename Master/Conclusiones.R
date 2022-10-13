0e161807a93d4bec8ca6a4b537f89ec4b2c0a873
unknown <mpa75@alu.ua.es>
2022-10-13 15:14:37 +0200

    anotaciones finales en comentarios

--- Master/Conclusiones.R
+++ Master/Conclusiones.R
@@ -1,7 +1,7 @@
 
 
 'Una vez terminada la práctica 1 de iniciación en R tengo que destacar que he echado en falta 
-mas documentación de apoyo para realizarla, ya que con los vídeos de youtube y el libro "The_Book_of_R", me ha sido insuficiente
+mas documentación de apoyo para realizarla, ya que con la ayuda de RStudio, los vídeos de youtube y el libro "The_Book_of_R", me ha sido insuficiente
 la obtención de todos los recursos para encontrar las soluciones a los problemas que aparecen durante su elaboración. 
 Por ese motivo hay puntos en la práctica donde sé que la solución no es la idónea, no la he obtenido ó no aparece, pero he incluido
 lo que mejor tengo en ese momento.
