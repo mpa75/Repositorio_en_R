d3025a3e24ad54f3b9bc9b448f05fdd778744003
unknown <mpa75@alu.ua.es>
2022-10-13 15:13:21 +0200

    Comentarios terminado

---
+++ Master/Conclusiones.R
@@ -0,0 +1,10 @@
+
+
+'Una vez terminada la práctica 1 de iniciación en R tengo que destacar que he echado en falta 
+mas documentación de apoyo para realizarla, ya que con los vídeos de youtube y el libro "The_Book_of_R", me ha sido insuficiente
+la obtención de todos los recursos para encontrar las soluciones a los problemas que aparecen durante su elaboración. 
+Por ese motivo hay puntos en la práctica donde sé que la solución no es la idónea, no la he obtenido ó no aparece, pero he incluido
+lo que mejor tengo en ese momento.
+Por lo demás me parece una buena idea hacer una práctica de iniciación en R. Espero que sea solo el inicio del aprendizaje en R, y que se pueda
+estudiar cosas más elaboradas en este software, como gráficos o tratamiento de datos más adelante. Eso sí, pedir que se
+incluya mucha más información de apoyo'
